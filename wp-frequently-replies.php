d25646215a77ec14f60d4a106e4c93246f5ac500
Hamid Reza Yazdani <yazdaniwp@gmaiil.com>
2024-05-06 12:00:40 +0330

    Add License tag and url to plugin header

--- wp-frequently-replies.php
+++ wp-frequently-replies.php
@@ -8,6 +8,8 @@
  * Text Domain: frequently-replies
  * Domain Path: /languages/
  * Requires PHP: 7.4
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  */
 
 defined( 'ABSPATH' ) || exit;
